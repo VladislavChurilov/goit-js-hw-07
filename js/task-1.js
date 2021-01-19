0e76a7fdf489b5fb00209659f2be429788786669
Vladislav Churilov <88vvch88@gmail.com>
2021-01-19 22:04:37 +0200

    дз 7

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+
+// const totalEl = el =>{
+//     const titleRef = document.querySelector('ul#categories');
+//     cons
+// }
+const listRef = document.querySelectorAll('.item');
+console.log(`В списке ${listRef.length} категории.`);
+
+// const titleRef = document.querySelectorAll('.item');
+// console.log(titleRef);
+// const firstCategory = document.querySelectorAll('');
+// console.log(firstCategory);
